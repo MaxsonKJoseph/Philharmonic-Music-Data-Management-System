6fd4898ce78c3b94a32c7b50e499f86696cee61e
Maxson K Joseph <83718009+MaxsonKJoseph@users.noreply.github.com>
2023-01-26 17:55:22 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -31,15 +31,32 @@ c.execute("""CREATE TABLE songs(
 c.execute("""CREATE TABLE album(
     album_name text,
     artist_name text,
-    FOREIGN KEY(artist_name) REFERENCES SONG(artist_name)
-    FOREIGN KEY(album_name) REFERENCES SONG(album_name)
+    song_name text,
+    release_date text,
+    FOREIGN KEY(artist_name) REFERENCES SONGS(artist_name),
+    FOREIGN KEY(album_name) REFERENCES SONGS(album_name),
+    FOREIGN KEY(song_name) REFERENCES SONGS(song_name),
+    FOREIGN KEY(release_date) REFERENCES SONGS(release_date)
     )""")
+
+
+c.execute("""CREATE TABLE artist(
+    song_name text,
+    artist_name text,
+    album_name text,
+    FOREIGN KEY(artist_name) REFERENCES SONGS(artist_name),
+    FOREIGN KEY(album_name) REFERENCES SONGS(album_name),
+    FOREIGN KEY(song_name) REFERENCES SONGS(song_name)
+    )""")
+
 c.execute("INSERT INTO admin (admin_name, password) VALUES ('Maxson', 'Maxson@123')")    
 c.execute("INSERT INTO admin (admin_name, password) VALUES ('admin', '123')")
 c.execute("INSERT INTO admin (admin_name, password) VALUES ('admin2', '223')")
 c.execute("INSERT INTO user (user_name, password) VALUES ('Sarkheel', '1223')")
 c.execute("INSERT INTO user (user_name, password) VALUES ('user', '123')")
 c.execute("INSERT INTO user (user_name, password) VALUES ('user1', '1233')")
+
+
 
 
 # Commit changes
